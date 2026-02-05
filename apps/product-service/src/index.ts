1215e7961ccf175ebeb2370818da31d8a37e0c6a
KusalChamu <chamuchamp09@gmail.com>
2026-02-05 10:56:16 +0530

    mongoDB connection configurations

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,11 +1,14 @@
-import express,{ Request,Response } from "express"
+import express,{ Request,Response,NextFunction } from "express"
 import cors from "cors"
 import { clerkMiddleware, getAuth } from '@clerk/express'
 import { shouldBeUser } from "./middleware/authMiddleware";
+import productRouter from "./routes/product.route"
+import categoryRouter from "./routes/category.route"
 
 const app = express();
 
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(clerkMiddleware());
 
 app.use(cors({
     origin:["http://localhost:3002","http://localhost:3003"],
@@ -24,6 +27,24 @@ app.get("/test",shouldBeUser,(req,res)=>{
    
     res.json({message:"Product service authenticated.",userId:req.userId})
 })
+
+app.use("/products",productRouter)
+app.use("/categories",categoryRouter)
+
+app.use((
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err);
+
+  return res
+    .status(err.status || 500)
+    .json({
+      message: err.message || "Internal server error",
+    });
+});
 
 app.listen(8000,()=>{
     console.log("app is running on port 8000");
