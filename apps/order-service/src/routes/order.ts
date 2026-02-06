e373a396a6e6c69ee8d09c3be458e2754c4b22b9
KusalChamu <chamuchamp09@gmail.com>
2026-02-06 14:36:55 +0530

    implement user roles

--- apps/order-service/src/routes/order.ts
+++ apps/order-service/src/routes/order.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify"
-import { shouldBeUser } from "../middleware/authMiddleware"
+import { shouldBeAdmin, shouldBeUser } from "../middleware/authMiddleware"
 import { Order } from "@repo/order-db"
 
 export const orderRoute = async (fastify:FastifyInstance)=>{
@@ -9,7 +9,7 @@ export const orderRoute = async (fastify:FastifyInstance)=>{
         
     })
 
-   fastify.get("/orders", async (request, reply) => {
+   fastify.get("/orders",{preHandler:shouldBeAdmin}, async (request, reply) => {
     const orders = await Order.find({});
     return reply.send(orders);
 });
